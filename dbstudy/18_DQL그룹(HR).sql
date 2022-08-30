61d34d802cb0deed8449e7bd7c54839b1c9f14be
duck_ach <djagmlfk@naver.com>
2022-08-30 16:12:36 +0900

    2022-08-30

--- dbstudy/18_DQL그룹(HR).sql
+++ dbstudy/18_DQL그룹(HR).sql
@@ -69,3 +69,76 @@ SELECT
     DEPARTMENT_ID
 HAVING
     COUNT(*) > 10;
+    
+    
+-- 연습
+-- 1. 급여평균이 10000 이상인 부서의 부서번호와 급여평균을 조회하기
+SELECT 
+       DEPARTMENT_ID AS 부서번호
+     , FLOOR(AVG(SALARY)) AS 급여평균
+  FROM 
+       EMPLOYEES
+ GROUP BY
+       DEPARTMENT_ID
+HAVING FLOOR(AVG(SALARY)) >= 10000;
+    
+    
+/* 2. 동일한 부서번호(DEPARTMENT_ID로 그룹화하기
+      동일한 부서번호를 가진 사원들을 직업아이디(JOB_ID)로 다시 그룹화하기
+      즉, 부서별 직업아이디별로 그룹화하기
+      부서별 직업아이디별로 그룹화하여 각 그룹의 사원 수 조회하기
+      부서번호가 없는 사원은 제외하기
+*/
+SELECT
+    DEPARTMENT_ID
+  , JOB_ID
+  , COUNT(*) AS 부서별사원수
+FROM
+    EMPLOYEES
+WHERE DEPARTMENT_ID IS NOT NULL
+GROUP BY DEPARTMENT_ID, JOB_ID
+ORDER BY 부서별사원수;
+
+-- DEPARTMENTS 테이블 연습 .
+
+-- 1. 동일한 지역(LOCATION_ID)으로 그룹화하여 조회하기
+SELECT
+    LOCATION_ID
+  FROM
+    DEPARTMENTS
+ GROUP BY LOCATION_ID;
+
+-- 2. 동일한 지역(LOCATION_ID)으로 그룹화하여 존재하는 부서수 조회하기
+--    부서수가 2 이상인 지역만 조회하기
+SELECT
+    LOCATION_ID
+  , COUNT(*) AS 존재하는부서수
+  FROM
+    DEPARTMENTS
+ GROUP BY LOCATION_ID
+ HAVING COUNT(*) >= 2;
+
+-- 3. 동일한 지역(LOCATION_ID)으로 그룹화하여 각 지역별 존재하는 부서수 조회하기
+--      MANAGER_ID가 없는 지역은 제외하고 조회하기
+SELECT
+    LOCATION_ID, COUNT(*) AS 존재하는부서수
+  FROM
+    DEPARTMENTS
+ WHERE MANAGER_ID IS NOT NULL
+ GROUP BY LOCATION_ID;
+
+-- 4. 부서명(DEPARTMENT_NAME)의 첫 2글자로 그룹화하여 해당 그룹의 개수 조회하기
+SELECT
+    SUBSTR(DEPARTMENT_NAME, 1, 2) AS 부서명첫2글자
+  , COUNT(*) AS 해당그룹의개수
+  FROM DEPARTMENTS
+ GROUP BY SUBSTR(DEPARTMENT_NAME, 1, 2);
+
+-- 5. 부서명(DEPARTMENT_NAME)의 첫 2글자로 그룹화하여 해당 그룹의 개수 조회하기
+-- 부서명의 첫 2글자가 'It', 'Co'인 부서만 조회하기
+SELECT
+    SUBSTR(DEPARTMENT_NAME, 1, 2), COUNT(*) AS 해당그룹의개수
+  FROM DEPARTMENTS
+-- WHERE SUBSTR(DEPARTMENT_NAME, 1, 2) IN('IT', 'Co') AS 해당그룹의개수
+ WHERE DEPARTMENT_NAME LIKE 'IT%' OR  DEPARTMENT_NAME LIKE'Co%'
+ GROUP BY SUBSTR(DEPARTMENT_NAME, 1, 2);
---
+++ dbstudy/19_조인(부서,사원).sql
@@ -0,0 +1,214 @@
+-- 테이블 삭제
+DROP TABLE EMPLOYEE;
+DROP TABLE DEPARTMENT;
+
+-- DEPARTMENT 테이블 생성
+CREATE TABLE DEPARTMENT(
+    DEPT_NO   NUMBER            NOT NULL,
+    DEPT_NAME VARCHAR2(15 BYTE) NOT NULL,
+    LOCATION  VARCHAR2(15 BYTE) NOT NULL
+);
+
+-- EMPLOYEE 테이블 생성
+CREATE TABLE EMPLOYEE(
+    EMP_NO    NUMBER            NOT NULL,
+    NAME      VARCHAR2(20 BYTE) NOT NULL,
+    DEPART    NUMBER            NULL,
+    POSITION  VARCHAR2(20 BYTE) NULL,
+    GENDER    CHAR(2)           NULL,
+    HIRE_DATE DATE              NULL, 
+    SALARY    NUMBER            NULL
+);
+
+-- 기본키
+ALTER TABLE DEPARTMENT 
+    ADD CONSTRAINT PK_DEPARTMENT PRIMARY KEY(DEPT_NO);
+ALTER TABLE EMPLOYEE 
+    ADD CONSTRAINT PK_EMPLOYEE PRIMARY KEY(EMP_NO);
+
+-- 외래키
+ALTER TABLE EMPLOYEE 
+    ADD CONSTRAINT FK_EMPLOYEE_DEPARTMENT FOREIGN KEY(DEPART) 
+        REFERENCES DEPARTMENT(DEPT_NO)
+            ON DELETE SET NULL;
+
+/*************************************************************/
+
+/*
+    시퀀스
+    1. SEQUENCE
+    2. 일련번호를 생성하는 데이터베이스 객체
+    3. 자동으로 증가하는 번호를 생성
+    4. 기본키(PK) 에서 주로 사용(인공키)
+    5. NEXTVAL를 이용하면 새로운 번호가 생성
+    6. CURRVAL를 이용하면 현재 번호를 확인
+*/
+
+/*
+    시퀀스 생성 형식
+    CREATE SEQUENCE 시퀀스_이름
+        START WITH 시작값                      -- 시작하면 1, 생성 이후 수정 불가
+        INCREMENT BY 증가값
+        MINVALUE 최소값
+        MAXVALUE 최대값
+        CACHE사용유무                          -- NOCACHE 권장
+        CYCLE사용유무                          
+    * 참고로 순서 상관없다.
+*/
+
+-- 부서 테이블에서 사용할 부서_시퀀스
+DROP SEQUENCE DEPARTMENT_SEQ;
+CREATE SEQUENCE DEPARTMENT_SEQ
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 100     -- NOMAXVALUE : 최대값없음
+    NOCACHE
+    NOCYCLE;         -- 테이블 하나당 시퀀스 하나씩 쓴다.
+
+-- 부서 테이블에 행(Row) 삽입
+-- 부모 테이블(관계에서 PK를 가진 테이블)에 먼저 삽입을 해야 함
+INSERT INTO DEPARTMENT(DEPT_NO, DEPT_NAME, LOCATION) VALUES (DEPARTMENT_SEQ.NEXTVAL , '영업부', '대구');
+INSERT INTO DEPARTMENT(DEPT_NO, DEPT_NAME, LOCATION) VALUES (DEPARTMENT_SEQ.NEXTVAL, '인사부', '서울');
+INSERT INTO DEPARTMENT(DEPT_NO, DEPT_NAME, LOCATION) VALUES (DEPARTMENT_SEQ.NEXTVAL, '총무부', '대구');
+INSERT INTO DEPARTMENT(DEPT_NO, DEPT_NAME, LOCATION) VALUES (DEPARTMENT_SEQ.NEXTVAL, '기획부', '서울');
+
+-- 작업의 완료
+COMMIT;
+
+-- 사원 테이블에서 사용할 사원_시퀀스
+DROP SEQUENCE EMPLOYEE_SEQ;
+CREATE SEQUENCE EMPLOYEE_SEQ
+    START WITH 1001
+    NOCACHE;
+    
+-- 사원 테이블에 행(Row) 삽입
+-- 자식 테이블(관계에서 FK를 가진 테이블)은 참조 무결성에 위배되지 않는 데이터만 삽입 가능
+-- 부서(부서번호) - 사원(소속부서)
+-- PK             - FK
+-- 1,2,3,4        - 1,2,3,4중 하나만 가능
+INSERT INTO EMPLOYEE VALUES (EMPLOYEE_SEQ.NEXTVAL, '구창민', 1, '과장', 'M', '95/05/01', 5000000);
+INSERT INTO EMPLOYEE VALUES (EMPLOYEE_SEQ.NEXTVAL, '김민서', 1, '사원', 'F', '17/09/01', 2000000);
+INSERT INTO EMPLOYEE VALUES (EMPLOYEE_SEQ.NEXTVAL, '이은영', 2, '부장', NULL, '90-09-01', 5500000);
+INSERT INTO EMPLOYEE VALUES (EMPLOYEE_SEQ.NEXTVAL, '한성일', 2, '과장', 'M', '93-04-01', 5000000);
+COMMIT;
+
+-- 참조무결성 위배 데이터 삽입을 위해서 외래키 일시중지
+ALTER TABLE EMPLOYEE
+    DISABLE CONSTRAINT FK_EMPLOYEE_DEPARTMENT;
+
+-- 참조무결성 위배 데이터 삽입
+INSERT INTO EMPLOYEE VALUES (EMPLOYEE_SEQ.NEXTVAL, '신현준', 5, '대리', 'M', '98-12-01', 3500000);
+COMMIT;
+
+/*
+    조인
+    1. JOIN
+    2. 2개 이상의 테이블을 조회하는 방법
+    3. 조회할 테이블들은 관계를 줄 수 있어야 함
+    4. 종류
+        1) 크로스 조인 : 카테젼 곱, 각 테이블에 모든 행을 조인
+           - CROSS JOIN
+           - 많은 행을 순식간에 만들 수 있음(기초데이터 작성용)
+           - 조인 조건을 잘못 지정한 경우
+        2) 내부 조인
+           - INNER JOIN
+           - 각 테이블에서 일치하는 모든 행을 조인
+        3) 외부 조인
+           - OUTER JOIN
+           - 한 테이블은 일치하는 행을 조인, 한 테이블은 일치하지 않아도 조인
+        4) 셀프 조인
+           - SELF JOIN 
+           - 한 테이블에 참조 관계가 있는 경우
+           - 한 테이블의 특정 칼럼과 다른 특정 칼럼을 조인
+    5. 형식
+        1) JOIN 문법
+            SELECT 칼럼
+              FROM 테이블1 JOIN 테이블2
+              ON 조인조건
+        2) 콤마(,) 문법
+            SELECT 칼럼
+              FROM 테이블1, 테이블2
+             WHERE 조인조건
+            
+*/
+SELECT EMP_NO, NAME, DEPT_NAME
+  FROM EMPLOYEE INNER JOIN DEPARTMENT
+    ON DEPART = DEPT_NO;
+
+
+
+
+/***********************************************************/
+
+DROP TABLE SAMPLE;
+CREATE TABLE SAMPLE(
+    NO1 NUMBER,
+    NO2 NUMBER
+);
+
+DROP SEQUENCE SAMPLE_SEQ;
+CREATE SEQUENCE SAMPLE_SEQ NOCACHE;
+
+-- 최초 1번은 NEXTVAL를 사용해야 CURRVAL도 사용 가능하다.
+INSERT INTO SAMPLE(NO1) VALUES(SAMPLE_SEQ.CURRVAL);
+
+-- NEXTVAL, CURRVAL 함께 사용
+INSERT INTO SAMPLE(NO1, NO2) VALUES(SAMPLE_SEQ.NEXTVAL, SAMPLE_SEQ.CURRVAL);
+COMMIT;
+
+
+-- 테이블 수정
+
+-- 1. 부서번호가 1인 부서의 지역을 '인천'으로 수정
+UPDATE DEPARTMENT
+   SET LOCATION = '인천'
+ WHERE DEPT_NO = 1;
+
+-- 2. 부서번호가 3인 부서명을 '전략부', 지역을 '부산'으로 수정
+UPDATE DEPARTMENT
+   SET DEPT_NAME = '전략부'
+     , LOCATION = '부산'
+ WHERE DEPT_NO = 3;
+
+/*
+    부서  -  사원
+    1     -  1
+    2     -  1
+    3     -  2
+    4     -  2
+    5     -  3
+*/
+-- 3. 부서번호가 3인 부서의 부서번호를 6으로 수정
+-- DEPARTMENT의 부서번호를 EMPLOYEE가 참조중이므로 수정이 안 됨
+-- 해결책
+-- 1. 외래키 일시중지
+-- 2. 수정
+-- 3. 외래키 재시작
+
+ALTER TABLE EMPLOYEE
+    DISABLE CONSTRAINT FK_EMPLOYEE_DEPARTMENT;  -- 외래키 중지
+
+UPDATE EMPLOYEE
+   SET DEPART = 6
+ WHERE DEPART = 3;
+
+UPDATE DEPARTMENT
+   SET DEPT_NO = 6
+ WHERE DEPT_NO = 3;
+
+ALTER TABLE EMPLOYEE
+    ENABLE CONSTRAINT FK_EMPLOYEE_DEPARTMENT;  -- 외래키 시작
+
+-- 4. 부서번호 1인 사원들의 월급을 100000 인상
+UPDATE EMPLOYEE
+   SET SALARY = SALARY + 100000
+ WHERE DEPART = 1;
+
+-- 5. 직급이 '과장'인 사원들의 월급을 10% 인상
+UPDATE EMPLOYEE
+   SET SALARY = SALARY * 1.1
+ WHERE POSITION = '과장';
+
+
+SELECT * FROM EMPLOYEE;
