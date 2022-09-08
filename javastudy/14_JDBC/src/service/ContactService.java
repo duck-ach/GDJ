257bc43d142c2d21cf628ed90a17d1ed7590a50b
duck_ach <djagmlfk@naver.com>
2022-09-08 16:13:24 +0900

    2022-09-08

---
+++ javastudy/14_JDBC/src/service/ContactService.java
@@ -0,0 +1,12 @@
+package service;
+
+import domain.ContactDTO;
+
+public interface ContactService {
+	public void addContact(ContactDTO contact);
+	public void modifyContact(ContactDTO contact);
+	public void deleteContact(int contact_no);
+	public void findContact(int contact_no);
+	public void findALLContacts();
+}
+
